6dda5e7f88a8b8033912d0b635349f9bd308bcbf
yaso07 <89770165+yaso07@users.noreply.github.com>
2024-08-29 12:32:49 +0530

    fix:model updates

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function SpotLightScene(props: any) {
     <>
       <spotLight
         castShadow
-        intensity={5}
+        intensity={4}
         ref={spotLightRef}
         angle={1.8}
         position={[0, 4.28, 1]}
@@ -147,7 +147,7 @@ function App() {
           <Canvas
             id="canvas"
             shadows
-            camera={{ position: [0, 1.1, 1.8], near: 0.001, far: 100 }}
+            camera={{ position: [1.5, 0, 4], near: 0.001, far: 100 }}
             style={{ height: "100vh", width: "100%", backgroundColor: "white" }}
           >
             {/* <ambientLight intensity={10} /> */}
@@ -311,10 +311,10 @@ function App() {
                   metalness={1}
                 ></meshStandardMaterial>
               </mesh> */}
-                  <CameraRig></CameraRig>
+              <CameraRig></CameraRig>
             </group>
             <PointerControls />
-      {/*       <OrbitControls></OrbitControls>*/}
+            {/* <OrbitControls></OrbitControls> */}
             {/* {/* <axesHelper args={[10]} /> */}
             {/* <gridHelper></gridHelper>  */}
           </Canvas>
